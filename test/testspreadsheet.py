e6e95db8a55c0135913cf08d78cb47cf6f7709b1
jonesiponesi <jonnaliu@student.oulu.fi>
2024-10-29 18:35:36 +0200

    US6 - Test 2

--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -77,3 +77,8 @@ class TestSpreadSheet(TestCase):
         spreadsheet.set("B1", "3")
         self.assertEqual(4, spreadsheet.evaluate("A1"))
 
+    def test_evaluate_with_invalid_arithmetic_operator_and_reference(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1", "=1+B1")
+        spreadsheet.set("B1", "3.1")
+        self.assertEqual("#Error", spreadsheet.evaluate("A1"))
